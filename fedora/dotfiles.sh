e8b04906871f43d3c3c6cfbe55dc1b31e046c0f4
sebastianstupak <sebastian.stupak@maethril.net>
2024-08-22 09:46:46 +0200

    fix nvim link

--- fedora/dotfiles.sh
+++ fedora/dotfiles.sh
@@ -20,7 +20,7 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 dotfiles_dir="$script_dir/../"  # Go one level up to /dotfiles
 
 # Set the path to the dotfiles' nvim, wezterm, and tmux directories
-nvim_config_source="$dotfiles_dir/nvim/init.lua"
+nvim_config_source="$dotfiles_dir/nvim"
 wezterm_config_source="$dotfiles_dir/wezterm/wezterm.lua"
 tmux_config_source="$dotfiles_dir/tmux/tmux.conf"
 
@@ -32,15 +32,17 @@ nvim_config_dest="$target_home/.config/nvim"
 wezterm_config_dest="$target_home/.config/wezterm"
 tmux_config_dest="$target_home/.config/tmux"
 
-# Create the Neovim configuration directory if it doesn't exist
-mkdir -p "$nvim_config_dest"
+# Remove existing Neovim configuration directory if it exists
+if [ -d "$nvim_config_dest" ]; then
+  rm -rf "$nvim_config_dest"
+fi
 
-# Link the init.lua from the dotfiles repository for Neovim
-if [ -f "$nvim_config_source" ]; then
-  ln -sf "$nvim_config_source" "$nvim_config_dest/init.lua"
-  echo "Neovim configuration linked successfully for user '$target_user'."
+# Link the entire nvim folder from the dotfiles repository
+if [ -d "$nvim_config_source" ]; then
+  ln -s "$nvim_config_source" "$nvim_config_dest"
+  echo "Neovim configuration folder linked successfully for user '$target_user'."
 else
-  echo "init.lua not found in $nvim_config_source!"
+  echo "Neovim configuration folder not found in $nvim_config_source!"
 fi
 
 # Create the WezTerm configuration directory if it doesn't exist
--- nvim/lua/sstupak/init.lua
+++ nvim/lua/sstupak/init.lua
@@ -1,2 +1,4 @@
 require("sstupak.remap")
 require("sstupak.set")
+require("sstupak.lazy")
+
